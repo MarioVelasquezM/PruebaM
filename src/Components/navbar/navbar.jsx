2d28443dba3d1aeeada94b1756f715256633caf7
MarioVelasquezM <marioan.velasquezm@gmail.com>
2023-08-12 22:45:19 -0500

    sda

--- src/Components/navbar/navbar.jsx
+++ src/Components/navbar/navbar.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import manita from "../../Assets/manita.jpg";
 
 const Navbar = () => {
-  return <h1 className="text-8xl">Hola</h1>;
+  return (
+    <nav>
+      <div className="flex-row items-center justify-center">
+        <img
+          src={manita}
+          alt="ManitaLogo"
+          className="rounded- boder-3 border-black"
+        />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
