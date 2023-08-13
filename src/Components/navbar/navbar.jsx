1b5151e1f266cda04968be9538772b7e27340b5e
MarioVelasquezM <marioan.velasquezm@gmail.com>
2023-08-12 22:18:12 -0500

    ada

--- src/Components/navbar/navbar.jsx
+++ src/Components/navbar/navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Navbar = () => {
-  return <h1 className="text-5xl text-gray-800">Hola</h1>;
+  return <h1 className="text-8xl">Hola</h1>;
   //Hola  >Hola</h1>
 };
 
