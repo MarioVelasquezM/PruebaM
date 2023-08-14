d6daf3753cdfda2423f5c0943a42755b9bd21ca1
MarioVelasquezM <marioan.velasquezm@gmail.com>
2023-08-13 22:25:29 -0500

    route 2

--- src/Components/MainView/main-view.jsx
+++ src/Components/MainView/main-view.jsx
@@ -6,52 +6,32 @@ import { useSearchParams } from "react-router-dom";
 
 const baseUrlApi = "https://api.mercadolibre.com/sites/MLA/search";
 
-const products = [
-  {
-    price: 2000,
-    details: "carrito de compras",
-    storeplace: "guanajuato",
-    id: 1,
-  },
-  {
-    price: 2000,
-    details: "carrito de compras",
-    storeplace: "guanajuato",
-    id: 2,
-  },
-  {
-    price: 4000,
-    details: "iaifone",
-    storeplace: "DF",
-    id: 3,
-  },
-  {
-    price: 2007,
-    details: "aifone 14pluexsd",
-    storeplace: "guanajuato",
-    id: 4,
-  },
-  {
-    price: 2007,
-    details: "aifone 14pluexsd",
-    storeplace: "guanajuato",
-    id: 47,
-  },
-];
-
 const MainView = () => {
-  // const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
   const [searchParams, _] = useSearchParams();
 
   useEffect(() => {
     const searchProduct = searchParams.get("search");
     const loadProduct = async () => {
       const response = await axios.get(`${baseUrlApi}?q=${searchProduct}#json`);
-      return response.data;
+      return response.data.results;
     };
 
     loadProduct()
-      .then()
+      .then((data) => {
+        const products = data.map((product) => {
+          return {
+            title: product.title,
+            price: product.price,
+            sellernicknamer: product.seller.nickname,
+            imageUrl: product.thumbnail,
+            id: product.id,
+            free_shipping: product.shipping.free_shipping,
+          };
+        });
+        console.log(products);
+        setProducts(products.slice(0, 4));
+      })
       .catch((error) => console.log(error));
   }, [searchParams]);
 
--- src/Components/Product/product-item.jsx
+++ src/Components/Product/product-item.jsx
@@ -1,24 +1,39 @@
 import React from "react";
-import logo from "../Assets/Logo_ML.png";
+import logo from "../Assets/ic_shipping.png";
 import "./product-item.css";
+import { useNavigate } from "react-router";
 /*const ProductItem = ('detail,price, Name,site, Envio') => */
+let usdolar = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "ARS",
+});
 
 const ProductItem = ({ product }) => {
+  const navigate = useNavigate();
+  const clickOveProduct = (e) => {
+    e.preventDefault();
+
+    navigate(`/items/${product.id}`);
+  };
+
   return (
     <div className="itemContainer">
       <div className="ContentProduct">
-        <div className="detailsProduct">
+        <div className="detailsProduct" onClick={clickOveProduct}>
           <div className="imageProduct">
-            <img src={logo} alt="logo" className="logoImage"></img>
+            <img src={product.imageUrl} alt="logo" className="logoImage"></img>
           </div>
 
           <div className="ProductInfo">
-            <h3 className="ItemPrice">{product.price}</h3>
-            <p className="ProductDescription">{product.details}</p>
+            <h3 className="ItemPrice">{usdolar.format(product.price)}</h3>
+            {product.free_shipping && (
+              <img src={logo} alt="freeshipping" className="freeshipping"></img>
+            )}
+            <p className="ProductDescription">{product.title}</p>
           </div>
         </div>
         <div>
-          <span>{product.storeplace}</span>
+          <p className="sellerName">{product.sellernicknamer}</p>
         </div>
       </div>
       <hr />
